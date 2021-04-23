0bfafc793eb86487938c51a031d57b61ca31717b
Ирина Марковщенкова <irinamrk02@mail.ru>
2021-04-23 21:45:48 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿
+namespace PR5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(415, 454);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(407, 426);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Обменом";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(400, 419);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.richTextBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(407, 426);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Выбором";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(3, 4);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(400, 419);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.richTextBox3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 24);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(407, 426);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Вставками";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(3, 4);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(400, 419);
+            this.richTextBox3.TabIndex = 1;
+            this.richTextBox3.Text = "";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.richTextBox4);
+            this.tabPage4.Location = new System.Drawing.Point(4, 24);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(407, 426);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Пузырьком";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(3, 4);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(400, 419);
+            this.richTextBox4.TabIndex = 1;
+            this.richTextBox4.Text = "";
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.AutoScroll = true;
+            this.tabPage5.Controls.Add(this.richTextBox5);
+            this.tabPage5.Location = new System.Drawing.Point(4, 24);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(407, 426);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Быстрая";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(3, 4);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(400, 419);
+            this.richTextBox5.TabIndex = 1;
+            this.richTextBox5.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(421, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Исходный массив:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(421, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(422, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Отсортировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Сортировки";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage5.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PR5
+{
+    public partial class Form1 : Form
+    {
+        static int[] mass = { 6, 8, 9, 2, 1, 3, 5, 4, 7 };
+        string type_of_sort = "Обменом";
+        private void sort()
+        {
+            check_mass();
+            switch (type_of_sort)
+            {
+                case "Обменом":
+                    richTextBox1.Text = "Обменом\n";
+                    GnomeSort(mass);
+                    break;
+                case "Выбором":
+                    richTextBox2.Text = "Выбором\n";
+                    SelectSort(mass);
+                    break;
+                case "Вставками":
+                    richTextBox3.Text = "Вставками\n";
+                    InsertSort(mass);
+                    break;
+                case "Пузырьком":
+                    richTextBox4.Text = "Пузырьком\n";
+                    BubbleSort(mass);
+                    break;
+                case "Быстрая":
+                    richTextBox5.Text = "Быстрая\n";
+                    QSort(mass,0,mass.Length-1,0);
+                    break;
+
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sort();
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            type_of_sort = tabControl1.SelectedTab.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            check_mass();
+        }
+        private void check_mass()
+        {
+            string[] mass_str = textBox1.Text.Split(" ");
+            mass = new int[mass_str.Length];
+            for (int i = 0; i < mass_str.Length; i++)
+            {
+                mass[i] = Convert.ToInt32(mass_str[i]);
+            }
+        }
+        static string WriteArray(int[] a, int iteration)
+        {
+            string arr = "\n"+iteration.ToString() + " итерация\n";
+            foreach (int k in a)
+            {
+                arr += k + " ";
+            }
+            return arr+"\n";
+        }
+        private void GnomeSort(int[] a) // edit for this project
+        {
+            int iteration = 0;
+            int i = 1;
+            int j = 2;
+            while (i < a.Length)
+            {
+                if (a[i - 1] < a[i])
+                {
+                    i = j;
+                    j = j + 1;
+                }
+                else
+                {
+                    Swap(a, i - 1, i);
+                    i = i - 1;
+                    if (i == 0)
+                    {
+                        i = j;
+                        j = j + 1;
+                    }
+                }
+                richTextBox1.Text += WriteArray(a, ++iteration);
+            }
+        }
+
+        private void SelectSort(int[] a) // edit for this project
+        {
+            int iteration = 0;
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a[j] < a[min])
+                    {
+                        min = j;
+                    }
+                }
+                int k = a[i];
+                a[i] = a[min];
+                a[min] = k;
+                richTextBox2.Text += WriteArray(a, ++iteration);
+            }
+        }
+
+        private void InsertSort(int[] a) // edit for this project
+        {
+            int iteration = 0;
+            for (int i = 1; i < a.Length; i++)
+            {
+                
+                int k = a[i];
+                int j = i;
+                while (j > 0 && k < a[j - 1])
+                {
+                    a[j] = a[j - 1];
+                    j--;
+                }
+                a[j] = k;
+                richTextBox3.Text += WriteArray(a, ++iteration);
+            }
+        }
+        private void BubbleSort(int[] a) // edit for this project
+        {
+            int iteration = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a[i] > a[j])
+                    {
+                        int o = a[i];
+                        a[i] = a[j];
+                        a[j] = o;
+                    }
+                }
+                richTextBox4.Text += WriteArray(a, ++iteration);
+            }
+        }
+        private void QSort(int[] arr, int l, int r, int iteration) // edit for this project
+        {
+            int _iteration = iteration++;
+            int i = l;
+            int j = r;
+            int x = arr[(l + r) / 2];
+            while (i <= j)
+            {
+                while (arr[i] < x) i++;
+                while (arr[j] > x) j--;
+                if (i <= j)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    i++;
+                    j--;
+                }
+                richTextBox5.Text += WriteArray(arr, ++_iteration);
+            }
+            if (l < j) QSort(arr, l, j, _iteration);
+            if (i < r) QSort(arr, i, r, _iteration);
+        }
+        static void Swap(int[] a, int i, int j)
+        {
+            int g = a[i];
+            a[i] = a[j];
+            a[j] = g;
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
